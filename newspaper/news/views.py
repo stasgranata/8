e5c9f71cbe9e7d41e608a3411d9e37cdb92c10fd
stasgranata <122440092+stasgranata@users.noreply.github.com>
2023-04-12 13:56:17 +0300

    Add files via upload

---
+++ newspaper/news/views.py
@@ -0,0 +1,79 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.core.exceptions import ValidationError
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .filters import PostFilter
+from .forms import PostForm
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-time_in'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+
+class SearchList(ListView):
+    model = Post
+    ordering = '-time_in'
+    template_name = 'search.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class PostCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('news.add_post')
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        if 'articles' in self.request.path:
+            post = form.save(commit=False)
+            post.is_article = True
+        return super().form_valid(form)
+
+
+class PostUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = ('news.change_post')
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        if ('news' in self.request.path) and post.is_article:
+            raise ValidationError(
+                "Вы пытаетесь редактировать статью как новость."
+            )
+        elif ('articles' in self.request.path) and not post.is_article:
+            raise ValidationError(
+                "Вы пытаетесь редактировать новость как статью."
+            )
+        else:
+            return super().form_valid(form)
+
+
+class PostDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('news.delete_post')
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
